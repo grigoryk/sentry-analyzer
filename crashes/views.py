5c09b4285bd7314cc35d3f837a3f025b878ed07b
Grisha Kruglov <gkruglov@mozilla.com>
2021-07-20 11:57:46 -0700

    cache computed packages, not the whole view

--- crashes/views.py
+++ crashes/views.py
@@ -1,32 +1,40 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from .models import Category
 
 import datetime
 from collections import defaultdict
 
-@cache_page(60 * 30)
 def packages(request):
-    packages = []
-    today = datetime.datetime.today()
-    cutoff_date = today - datetime.timedelta(days=29)
-    date_list = [(today - datetime.timedelta(days=x)).date() for x in range(30)]
+    packages = cache.get('computed_packages')
+    if not packages is None:
+        return render(request, 'crashes/packages.html', {
+            'packages': packages
+        })
+    else:
+        packages = []
+        today = datetime.datetime.today()
+        cutoff_date = today - datetime.timedelta(days=29)
+        date_list = [(today - datetime.timedelta(days=x)).date() for x in range(30)]
 
-    for p in Category.objects.all():
-        package = {
-            "name": p.name,
-            "dates": {d.isoformat(): {'info': 0, 'fatal': 0} for d in date_list}
-        }
-        for group in p.eventgroup_set.all():
-            for event in group.event_set.filter(event_created__gte=cutoff_date):
-                if event.is_info():
-                    package['dates'][event.event_created.date().isoformat()]['info'] += 1
-                else:
-                    package['dates'][event.event_created.date().isoformat()]['fatal'] += 1
-        packages.append(package)
-    return render(request, 'crashes/packages.html', {
-        'packages': packages
-    })
+        for p in Category.objects.all():
+            package = {
+                "name": p.name,
+                "dates": {d.isoformat(): {'info': 0, 'fatal': 0} for d in date_list}
+            }
+            for group in p.eventgroup_set.all():
+                for event in group.event_set.filter(event_created__gte=cutoff_date):
+                    if event.is_info():
+                        package['dates'][event.event_created.date().isoformat()]['info'] += 1
+                    else:
+                        package['dates'][event.event_created.date().isoformat()]['fatal'] += 1
+            packages.append(package)
+
+        cache.set('computed_packages', packages, 60 * 30)
+
+        return render(request, 'crashes/packages.html', {
+            'packages': packages
+        })
 
 def package(request, package_name):
     package = Category.objects.get(name=package_name)
