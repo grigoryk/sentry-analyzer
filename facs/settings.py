6b0767ca9c0779e3c210a2170c24d65e6a457816
Grisha Kruglov <gkruglov@mozilla.com>
2021-08-11 21:01:19 -0700

    fix local env

--- facs/settings.py
+++ facs/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'insecuregooblygoob')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
