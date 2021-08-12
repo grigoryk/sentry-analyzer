c30d5b58b0ee00d4e4758340b6cbb551b1f651d3
Grisha Kruglov <gkruglov@mozilla.com>
2021-08-11 20:44:27 -0700

    remove secret_key

--- facs/settings.py
+++ facs/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&hk8^@!_40&m!-@r^-p9l$6kqy^v($)dhcfp)xo)9i0wy93mpt'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
