165c9d0ac2c4edae283dea9d66704339331baffa
Grisha Kruglov <gkruglov@mozilla.com>
2021-08-11 20:17:42 -0700

    heroku db config

--- facs/settings.py
+++ facs/settings.py
@@ -74,14 +74,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'facs.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
@@ -139,6 +134,9 @@ CELERY_RESULT_BACKEND = 'django-db'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 try:
     from local_settings import *
